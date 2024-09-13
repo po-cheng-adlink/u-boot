0e12bf57db2a8b22be1cffda067bfe7a3d91a981
po.cheng <po.cheng@adlinktech.com>
2024-09-13 11:43:31 +0800

    lib: string: Add memmem library function
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- include/linux/string.h
+++ include/linux/string.h
@@ -125,6 +125,9 @@ extern int memcmp(const void *,const void *,__kernel_size_t);
 #ifndef __HAVE_ARCH_MEMCHR
 extern void * memchr(const void *,int,__kernel_size_t);
 #endif
+#ifndef __HAVE_ARCH_MEMMEM
+extern void * memmem(const void *,__kernel_size_t,const void *,__kernel_size_t);
+#endif
 #ifndef __HAVE_ARCH_MEMCHR_INV
 void *memchr_inv(const void *, int, size_t);
 #endif
