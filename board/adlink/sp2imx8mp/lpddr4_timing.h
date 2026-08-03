f785b0b259bd1acb8308371d41f17d7691ce3b0c
po.cheng <po.cheng@adlinktech.com>
2026-08-03 15:21:03 +0800

    sp2imx8mp: copy from imx8mp_evk and modify lpddr4 training for SPL
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

---
+++ board/adlink/sp2imx8mp/lpddr4_timing.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2020 Marek Vasut <marek.vasut@gmail.com>
+ */
+
+#ifndef __LPDDR4_TIMING_H__
+#define __LPDDR4_TIMING_H__
+
+extern struct dram_timing_info dram_timing_2G;
+extern struct dram_timing_info dram_timing_4G;
+extern struct dram_timing_info dram_timing_8G;
+
+#endif /* __LPDDR4_TIMING_H__ */
