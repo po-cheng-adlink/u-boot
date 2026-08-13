904695a7c7f1d16fdcda429b13b5ad676049b151
po.cheng <po.cheng@adlinktech.com>
2026-08-13 16:55:19 +0800

    sp2imx8mp: LDB: Modify for Bridge Operations
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- drivers/video/nxp/imx/ldb.c
+++ drivers/video/nxp/imx/ldb.c
@@ -32,12 +32,20 @@
 #define LVDS_CTRL_PRE_EMPH_ADJ(n) (((n) & 0x7) << 5)
 #define LVDS_CTRL_CC_ADJ(n) (((n) & 0x7) << 11)
 
+enum ENUM_BITMAPPING {
+	SPWG = 0,
+	JEIDA,
+	UNKNOWN = -1,
+};
+
 struct imx_ldb_priv {
 	struct clk ldb_clk;
 	void __iomem *ldb_ctrl;
 	void __iomem *lvds_ctrl;
 	struct udevice *lvds1;
 	struct udevice *lvds2;
+	unsigned int data_width;
+	enum ENUM_BITMAPPING data_mapping;
 };
 
 static int imx_ldb_set_backlight(struct udevice *dev, int percent)
@@ -45,10 +53,12 @@ static int imx_ldb_set_backlight(struct udevice *dev, int percent)
 	struct imx_ldb_priv *priv = dev_get_priv(dev);
 	int ret;
 
+	debug("%s(): ldb: set backlight\n", __func__);
+
 	if (priv->lvds1) {
 		ret = panel_enable_backlight(priv->lvds1);
 		if (ret) {
-			debug("ldb: Cannot enable lvds1 backlight\n");
+			debug("%s(): ldb: Cannot enable lvds1 backlight\n", __func__);
 			return ret;
 		}
 
@@ -60,7 +70,7 @@ static int imx_ldb_set_backlight(struct udevice *dev, int percent)
 	if (priv->lvds2) {
 		ret = panel_enable_backlight(priv->lvds2);
 		if (ret) {
-			debug("ldb: Cannot enable lvds2 backlight\n");
+			debug("%s(): ldb: Cannot enable lvds2 backlight\n", __func__);
 			return ret;
 		}
 
@@ -80,10 +90,12 @@ static int imx_ldb_of_to_plat(struct udevice *dev)
 	uclass_get_device_by_endpoint(UCLASS_PANEL, dev, 1, -1, &priv->lvds1);
 	uclass_get_device_by_endpoint(UCLASS_PANEL, dev, 2, -1, &priv->lvds2);
 	if (!priv->lvds1 && !priv->lvds2) {
-		debug("ldb: No remote panel for '%s' (ret=%d)\n",
+		debug("%s(): ldb: No remote panel for '%s' (ret=%d)\n", __func__,
 		      dev_read_name(dev), ret);
 		return ret;
 	}
+
+	debug("%s(): ldb: got panels: 0x%lx 0x%lx\n", __func__, priv->lvds1, priv->lvds2);
 
 	return 0;
 }
@@ -103,18 +115,19 @@ static ulong imx_ldb_input_rate(struct imx_ldb_priv *priv,
 	return IMX_LDB_INTERNAL_DIVISOR(target_rate);
 }
 
-static int imx_ldb_attach(struct udevice *dev)
+static int imx_ldb_check_timing(struct udevice *dev, struct display_timing *timing)
 {
 	struct imx_ldb_priv *priv = dev_get_priv(dev);
 	struct display_timing timings;
-	bool format_jeida = false;
-	bool format_24bpp = true;
+
 	u32 ldb_ctrl = 0, lvds_ctrl;
 	ulong ldb_rate;
 	int ret;
 
-	/* TODO: update the 24bpp/jeida booleans with proper checks when they
-	 * will be supported.
+	/*
+	 * Not all pixel clocks will work, as the final rate (after internal
+	 * integer division) should be identical to the LCDIF clock, otherwise
+	 * the rendering will appear resized/shimmering.
 	 */
 	if (priv->lvds1) {
 		ret = panel_get_display_timing(priv->lvds1, &timings);
@@ -128,12 +141,13 @@ static int imx_ldb_attach(struct udevice *dev)
 		}
 
 		ldb_ctrl |= LDB_CTRL_CH0_ENABLE;
-		if (format_24bpp)
+		if (priv->data_width == 24)
 			ldb_ctrl |= LDB_CTRL_CH0_DATA_WIDTH;
-		if (format_jeida)
+		if (priv->data_mapping == JEIDA)
 			ldb_ctrl |= LDB_CTRL_CH0_BIT_MAPPING;
 		if (timings.flags & DISPLAY_FLAGS_VSYNC_HIGH)
 			ldb_ctrl |= LDB_CTRL_DI0_VSYNC_POLARITY;
+		debug("%s(): ldb: lvds1: ldb_ctrl: 0x%lx\n", __func__, ldb_ctrl);
 	}
 
 	if (priv->lvds2) {
@@ -148,27 +162,36 @@ static int imx_ldb_attach(struct udevice *dev)
 		}
 
 		ldb_ctrl |= LDB_CTRL_CH1_ENABLE;
-		if (format_24bpp)
+		if (priv->data_width == 24)
 			ldb_ctrl |= LDB_CTRL_CH1_DATA_WIDTH;
-		if (format_jeida)
+		if (priv->data_mapping == JEIDA)
 			ldb_ctrl |= LDB_CTRL_CH1_BIT_MAPPING;
 		if (timings.flags & DISPLAY_FLAGS_VSYNC_HIGH)
 			ldb_ctrl |= LDB_CTRL_DI1_VSYNC_POLARITY;
+		debug("%s(): ldb: lvds2: ldb_ctrl: 0x%lx\n", __func__, ldb_ctrl);
 	}
 
-	/*
-	 * Not all pixel clocks will work, as the final rate (after internal
-	 * integer division) should be identical to the LCDIF clock, otherwise
-	 * the rendering will appear resized/shimmering.
-	 */
 	ldb_rate = imx_ldb_input_rate(priv, &timings);
 	clk_set_rate(&priv->ldb_clk, ldb_rate);
 
 	writel(ldb_ctrl, priv->ldb_ctrl);
+	debug("%s(): ldb: ldb_rate: 0x%lx\n", __func__, ldb_rate);
 
+	return 0;
+}
+
+static int imx_ldb_attach(struct udevice *dev)
+{
+	struct imx_ldb_priv *priv = dev_get_priv(dev);
+	u32 lvds_ctrl;
+
+	debug("%s(): ldb: attach data-width:%d data-mapping:%d\n", __func__, priv->data_width, priv->data_mapping);
+
+	/* Phy Power On */
 	lvds_ctrl = LVDS_CTRL_CC_ADJ(2) | LVDS_CTRL_PRE_EMPH_EN |
 		    LVDS_CTRL_PRE_EMPH_ADJ(3) | LVDS_CTRL_VBG_EN;
 	writel(lvds_ctrl, priv->lvds_ctrl);
+	debug("%s(): ldb: power on: lvds_ctrl: 0x%lx\n", __func__, lvds_ctrl);
 
 	/* Wait for VBG to stabilize. */
 	udelay(15);
@@ -179,8 +202,24 @@ static int imx_ldb_attach(struct udevice *dev)
 		lvds_ctrl |= LVDS_CTRL_CH1_EN;
 
 	writel(lvds_ctrl, priv->lvds_ctrl);
+	debug("%s(): ldb: power on(en): lvds_ctrl: 0x%lx\n", __func__, lvds_ctrl);
 
 	return 0;
+}
+
+static int of_get_data_mapping(struct udevice *dev)
+{
+    const char *bm;
+
+    bm = dev_read_string(dev, "fsl,data-mapping");
+    if (bm == NULL)
+        return UNKNOWN;
+	else if (!strcasecmp(bm, "spwg"))
+		return SPWG;
+	else if (!strcasecmp(bm, "jeida"))
+		return JEIDA;
+
+    return UNKNOWN;
 }
 
 static int imx_ldb_probe(struct udevice *dev)
@@ -189,6 +228,8 @@ static int imx_ldb_probe(struct udevice *dev)
 	struct udevice *parent = dev_get_parent(dev);
 	fdt_addr_t parent_addr, child_addr;
 	int ret;
+
+	debug("\n%s(): driver %s\n", __func__, dev->driver->name);
 
 	ret = clk_get_by_name(dev, "ldb", &priv->ldb_clk);
 	if (ret < 0)
@@ -214,6 +255,19 @@ static int imx_ldb_probe(struct udevice *dev)
 	if (!priv->lvds_ctrl)
 		return -EINVAL;
 
+	/* fsl,data-width */
+	ret = dev_read_u32(dev, "fsl,data-width", &priv->data_width);
+	if (ret || (priv->data_width != 18 && priv->data_width != 24)) {
+		debug("%s(): data width not set or invalid, force default to 24\n", __func__);
+		priv->data_width = 24;
+	}
+	/* fsl,data-mapping */
+	priv->data_mapping = of_get_data_mapping(dev);
+	if (priv->data_mapping < 0) {
+		debug("%s(): data map not set or invalid, force default to spwg\n", __func__);
+		priv->data_mapping = SPWG;
+	}
+
 	ret = clk_enable(&priv->ldb_clk);
 	if (ret)
 		return ret;
@@ -232,6 +286,7 @@ dis_clk:
 
 struct video_bridge_ops imx_ldb_ops = {
 	.attach = imx_ldb_attach,
+	.check_timing = imx_ldb_check_timing,
 	.set_backlight	= imx_ldb_set_backlight,
 };
 
